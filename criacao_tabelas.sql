9baaf583a61612a8d6e77c29ed0beea0f73c613f
Mateus Wolke <61051738+wolke412@users.noreply.github.com>
2024-06-05 14:52:10 -0300

    sync

--- criacao_tabelas.sql
+++ criacao_tabelas.sql
@@ -1,26 +1,13 @@
--- Requisiçãso schema ser chamado trabalho.
 CREATE SCHEMA trabalho;
 SET search_path = trabalho;
-
--- ALTERAÇÕES SIGNIFICANTES
-
--- Todas as PKs foram convertidas INT -> SERIAL (é o tipo com AUTO INCREMENT no PG);
--- Todas as variaveis de data foram convertidas pra TIMESTAMP;
--- Alguns atributos foram passados de DOUBLE PRECISION -> DECIMAL(X,Y)
---   |: X digitos antes do ponto, Y digitos depois :| 
---   Outros foram convertidos pra REAL (float normal do pg).
---   // 
---   DOUBLE PRECISION aumenta o numero de casas decimais só; 
---   Precisamos de numero grandes na maioria das vezes.
-
 
 CREATE TABLE jogador(
 	id_jogador SERIAL PRIMARY KEY,
 	nome VARCHAR(30),
 	email VARCHAR(30),
 	criado_em TIMESTAMP,
-
-	saldo DECIMAL(50, 2) -- O jogo é baseado em ter muito dinheiro, precisamos de muito espaço nesse campo;
+	
+	saldo DECIMAL(50, 2)
 );
 
 CREATE TABLE tipo_investimento (
@@ -28,11 +15,11 @@ CREATE TABLE tipo_investimento (
 	nome VARCHAR(30),
 
 	valor_aquisicao REAL,
-	rendimento_ciclo REAL, -- Valor base
+	rendimento_ciclo REAL, 
 
-	tempo_ciclo_sec SMALLINT, -- tempo do ciclo em segundos. Pode ser pequeno.
-	gr_rendimento REAL,       -- growth rate rendimento; float - 100% -> dobra de valor a cada nível  
-	gr_preco REAL             -- growth rate do preço; flaot - 100% -> dobra o preço a cada nível
+	tempo_ciclo_sec SMALLINT, 
+	gr_rendimento REAL,       
+	gr_preco REAL            
 );
 
 CREATE TABLE gestor(
@@ -40,42 +27,33 @@ CREATE TABLE gestor(
 	id_tipo_inv INT,
 
 	nome VARCHAR(30),
-	custo_aquisicao DECIMAL(10, 2), -- Grande mas não tão grande;
+	custo_aquisicao DECIMAL(10, 2), 
 
-	comissao_venda REAL,-- seria porcentagem em decimal?
-		-- Pro nosso caso sim; representa melhor a ideia que queremos.
+	comissao_venda REAL,
 
 	CONSTRAINT fk_tipo_investimento 
-		FOREIGN KEY (id_tipo_inv) REFERENCES tipo_investimento (id_tipo_investimento)
+		FOREIGN KEY (id_tipo_inv) REFERENCES tipo_investimento (id_tipo_investimento) ON DELETE CASCADE
  );
  
 
--- Essa tabela vai fugir à regra dos três campos com tipos diferentes...
--- Teríamos que pensar em alguma coisa a mais pra pôr aqui (criado_em...)
 CREATE TABLE atributos_melhoraveis (
 	id_atributo SERIAL PRIMARY KEY,
-	-- O nome é meramente ilustrativo.
-	nome VARCHAR(20) -- GROWTH_RATE, PRICE_GROWTH_RATE, CYCLE_TIME ... 
+	nome VARCHAR(20) 
 );
 
 CREATE TABLE tipo_melhoria(
  	id_tipo_melhoria SERIAL PRIMARY KEY,
 	id_tipo_inv INT,
 	nome VARCHAR(30),
-	descr VARCHAR(200), -- "desc" é palavra reservada 
+	descr VARCHAR(200), 
 
 	id_atributo_melhorado INT,
-	taxa_alteracao REAL, -- Essa nunca vai ser um numero grande, nem com muitas casas decimais;
-	-- Ex.: 
-	--	[
-	--		taxa_alteracao: -10.0;
-	--		id_atri...    : 2 (PRICE_GROWTH_RATE); 
-	--	] ; A taxa de crescimento de preço reduzirá em 10%; 90% -> 81%;
+	taxa_alteracao REAL, 
 
 	CONSTRAINT fk_tipo_investimento 
-		FOREIGN KEY (id_tipo_inv) REFERENCES tipo_investimento (id_tipo_investimento),
+		FOREIGN KEY (id_tipo_inv) REFERENCES tipo_investimento (id_tipo_investimento) ON DELETE CASCADE,
 	CONSTRAINT fk_tipo_atributo
-		FOREIGN KEY (id_atributo_melhorado) REFERENCES atributos_melhoraveis (id_atributo)
+		FOREIGN KEY (id_atributo_melhorado) REFERENCES atributos_melhoraveis (id_atributo) ON DELETE CASCADE
 );
 
 
@@ -91,16 +69,16 @@ CREATE TABLE investimentos(
 	ultimo_ciclo_iniciou_em TIMESTAMP,
 
 	CONSTRAINT fk_id_jogador
-		FOREIGN KEY (id_jogador) REFERENCES jogador (id_jogador),
+		FOREIGN KEY (id_jogador) REFERENCES jogador (id_jogador) ON DELETE CASCADE,
 	CONSTRAINT fk_id_investimento
-		FOREIGN KEY (id_tipo_inv) REFERENCES tipo_investimento (id_tipo_investimento)
+		FOREIGN KEY (id_tipo_inv) REFERENCES tipo_investimento (id_tipo_investimento) ON DELETE CASCADE
 );
 
 CREATE TABLE rendimentos(
 	id_rendimento SERIAL PRIMARY KEY,
 	id_inv INT,
 
-	valor_obtido DECIMAL(20, 2), -- Pode ser gigante em algum ponto do jogo
+	valor_obtido DECIMAL(20, 2), 
 
 	nivel_inv INT,
 
@@ -108,7 +86,7 @@ CREATE TABLE rendimentos(
 	coletado_em TIMESTAMP,
 
 	CONSTRAINT fk_id_investimento 
-	FOREIGN KEY (id_inv) REFERENCES investimentos (id_inv)
+	FOREIGN KEY (id_inv) REFERENCES investimentos (id_inv) ON DELETE CASCADE
 );
 
 CREATE TABLE contratacao_gestor(
@@ -118,9 +96,9 @@ CREATE TABLE contratacao_gestor(
 	contratado_em TIMESTAMP,
 
 	CONSTRAINT fk_id_jogador
-		FOREIGN KEY (id_jogador) REFERENCES jogador (id_jogador),
+		FOREIGN KEY (id_jogador) REFERENCES jogador (id_jogador) ON DELETE CASCADE,
 	CONSTRAINT fk_id_gestor
-		FOREIGN KEY (id_gestor) REFERENCES gestor (id_gestor)
+		FOREIGN KEY (id_gestor) REFERENCES gestor (id_gestor) ON DELETE CASCADE
 );
 
 CREATE TABLE melhorias(
@@ -130,16 +108,16 @@ CREATE TABLE melhorias(
 	adquirido_em TIMESTAMP,
 
 	CONSTRAINT fk_id_tipo_melhoria
-		FOREIGN KEY (id_tipo_melhoria) REFERENCES tipo_melhoria (id_tipo_melhoria),
+		FOREIGN KEY (id_tipo_melhoria) REFERENCES tipo_melhoria (id_tipo_melhoria) ON DELETE CASCADE,
 	CONSTRAINT fk_id_jogador
-		FOREIGN KEY (id_jogador) REFERENCES jogador (id_jogador)
+		FOREIGN KEY (id_jogador) REFERENCES jogador (id_jogador) ON DELETE CASCADE
 );
 
 
 CREATE TABLE tipo_item(
 	id_tipo_item SERIAL PRIMARY KEY,
 	nome VARCHAR(30),
-	preco_aquisicao DECIMAL(50, 2), -- Esperemos que os itens aumentarão radicalmamente de preço
+	preco_aquisicao DECIMAL(50, 2), 
 	quant_vendas INT
 );
 
@@ -150,13 +128,11 @@ CREATE TABLE itens(
 	posicao_inventario INT,
 
 	esta_em_destaque VARCHAR(1) CHECK (esta_em_destaque IN ('s', 'n')),
-	-- Não vou mexer nessa pq ta muito elegante;
-	-- Mas poderia ser só um BOOLEAN; 
 
 	obtido_em TIMESTAMP,
 
 	CONSTRAINT fk_tipo_item 
-		FOREIGN KEY (id_tipo_item) REFERENCES tipo_item (id_tipo_item),
+		FOREIGN KEY (id_tipo_item) REFERENCES tipo_item (id_tipo_item) ON DELETE CASCADE,
 	CONSTRAINT fk_id_jogador
-		FOREIGN KEY (id_jogador) REFERENCES jogador (id_jogador)
+		FOREIGN KEY (id_jogador) REFERENCES jogador (id_jogador) ON DELETE CASCADE
 );
