5513b992b7971974e93adc01c6128738da58e57b
Mateus Wolke <61051738+wolke412@users.noreply.github.com>
2024-06-06 20:32:30 -0300

    added tipo_investimentos

--- insertions.sql
+++ insertions.sql
@@ -21,9 +21,17 @@ VALUES
 -- +------------------------------------
 -- |
 -- | Insere 5 tipos de investimentos.
+-- |
 
 INSERT INTO tipo_investimento 
-    ( nome, valor_aquisicao, rendimento_ciclo, tempo_ciclo_sec, gr_rendimento, gr_preco REAL )
+    ( nome, valor_aquisicao, rendimento_ciclo, tempo_ciclo_sec, gr_rendimento, gr_preco )
 VALUES 
-    ( "Carrinho de Picolé", 0, 10, 10, 10.0, 50.00 ), -- Todo jogador deve possuir o "investimento inicial".
-    -- ...
+    ( "Carrinho de Picolé",     100,     10,  5,  10.0,  50.00 ), -- A primeira ação de um jogador deverá ser comprar o investimento incial.
+    ( "Lavagem de carro"  ,    1000,    300, 10,  30.0, 100.00 ),
+    ( "Mercado"           ,   10000,   1000, 15,  80.0, 150.00 ),
+    ( "Shopping"          ,   50000,  10000, 30, 190.0, 200.00 ),
+    ( "Banco"             , 1000000, 500000, 60,  50.0,  50.00 ),
+
+-- |
+-- +------------------------------------
+
