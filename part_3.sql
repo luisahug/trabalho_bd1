6a283fde28f435e505434daca7d29c0bdc1f7725
Luisa <122617494+luisahug@users.noreply.github.com>
2024-06-16 10:41:32 -0300

    Add files via upload

--- part_3.sql
+++ part_3.sql
@@ -5,21 +5,22 @@
 -- | // Insira comentário aqui.
 -- | 
 
-    SELECT * FROM jogador
+    SELECT * FROM jogador;
 
 -- | 
 -- +------------------------------------
-
 
 -- [C]----------------------------------
 -- | Gerar uma consulta que envolva duas tabelas 
 -- | usando equi-join 
 -- +------------------------------------
 -- | 
--- | // Insira comentário aqui.
+-- | // Relaciona investimentos com gestores que trabalham com o mesmo tipo de investimento.
 -- | 
 
--- Código SQL aqui
+    SELECT investimentos.*, gestor.*
+    FROM investimentos, gestor
+    WHERE investimentos.id_tipo_inv = gestor.id_tipo_inv;
 
 -- | 
 -- +------------------------------------
@@ -30,10 +31,13 @@
 -- | usando inner-join 
 -- +------------------------------------
 -- | 
--- | // Insira comentário aqui.
+-- | // Exibe os itens dos usuários e as informações de cada tipo de item
 -- | 
 
--- Código SQL aqui
+    SELECT itens.*, tipo_item.*
+    FROM itens
+    INNER JOIN tipo_item
+    USING itens.id_tipo_item = tipo_item.id_tipo_item;
 
 -- | 
 -- +------------------------------------
@@ -58,10 +62,13 @@
 -- | usando inner-join 
 -- +------------------------------------
 -- | 
--- | // Insira comentário aqui.
+-- | // Exibe as contratações com os nomes do jogador e gestor envolvidos.
 -- | 
 
--- Código SQL aqui
+    SELECT cg.id_contratacao, j.nome AS 'jogador', g.nome AS 'gestor', cg.contratado_em
+    FROM contratacao_gestor cg
+    INNER JOIN jogador j ON cg.id_jogador = j.id_jogador
+    INNER JOIN gestor g ON cg.id_gestor = g.id_gestor;
 
 -- | 
 -- +------------------------------------
@@ -86,10 +93,14 @@
 -- | NULL, envolvendo no mínimo 2 tabelas 
 -- +------------------------------------
 -- | 
--- | // Insira comentário aqui.
+-- | // Exibe os investimentos que já foram coletados ao menos uma vez, e que já
+--      renderam mais que rendimento por ciclo
 -- | 
 
--- Código SQL aqui
+    SELECT inv.*, ti.nome AS 'tipo investimento', ti.rendimento_ciclo
+    FROM investimentos inv
+    INNER JOIN tipo_investimento ti ON inv.id_tipo_investimento = ti.id_tipo_investimento
+    WHERE inv.ultima_coleta_em IS NOT NULL AND inv.rendimento_atual > ti.rendimento_ciclo;
 
 -- | 
 -- +------------------------------------
@@ -101,10 +112,13 @@
 -- | 2tabelas 
 -- +------------------------------------
 -- | 
--- | // Insira comentário aqui.
+-- | // Exibe os investimentos feitos pelo jogador com id '3' no mês de jan/2024
 -- | 
 
--- Código SQL aqui
+    SELECT inv.* 
+    WHERE investimentos inv 
+    INNER JOIN jogador j ON inv.id_jogador = j.id_jogador
+    WHERE inv.id_jogador = 3 AND inv.obtido_em LIKE '2024-01-%';
 
 -- | 
 -- +------------------------------------
@@ -215,10 +229,11 @@
 -- | Implementar uma consulta usando a cláusula DELETE 
 -- +------------------------------------
 -- | 
--- | // Insira comentário aqui.
+-- | Exclui o jogador núm. 5 da tabela de jogadores
 -- | 
 
--- Código SQL aqui
+    DELETE FROM jogador
+    WHERE id_jogador = 5;
 
 -- | 
 -- +------------------------------------
@@ -229,10 +244,14 @@
 -- | atualizando mais de dois atributos 
 -- +------------------------------------
 -- | 
--- | // Insira comentário aqui.
+-- | Atualiza os dados de um investimento de id 2
 -- | 
 
--- Código SQL aqui
+    UPDATE investimentos
+    SET nivel_atual = 3,
+        preco_atual = 234002.45,
+        ultimo_ciclo_iniciou_em = CURRENT_TIMESTAMP
+    WHERE id_inv = 2;
 
 -- | 
 -- +------------------------------------
@@ -245,7 +264,11 @@
 -- | // Insira comentário aqui.
 -- | 
 
--- Código SQL aqui
+    --primeiro remover coluna que usa a PK da tabela do drop como FK
+    ALTER TABLE tipo_melhoria
+    DELETE COLUMN id_atributo_melhorado; 
+
+    DROP atributos_melhoraveis;
 
 -- | 
 -- +------------------------------------
